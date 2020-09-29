fe0aa343195aead342c6538273f15c77c249795f
extragravee <singhsidak@ymail.com>
2020-09-29 23:38:48 +1000

    market maker seems okay on 1017

--- PyCharm Projects/3. assignment_2/CAPMBot.py
+++ PyCharm Projects/3. assignment_2/CAPMBot.py
@@ -13,6 +13,7 @@ PLEASE READ:
     2. Notes sold when cash drops below arbitrary threshold.
 """
 import copy
+import logging
 import itertools
 from enum import Enum
 from typing import List
@@ -34,6 +35,8 @@ WAIT_10_SECONDS = 10
 MIN_CASH_THRESHOLD = 500
 NOTE_SELLING_PRICE = 495
 
+PROFIT_MARGIN = 50
+
 
 # Bot type enum
 class BotType(Enum):
@@ -44,7 +47,7 @@ class BotType(Enum):
 class CAPMBot(Agent):
 
     def __init__(self, account, email, password, marketplace_id,
-            risk_penalty=0.001, session_time=20, aggressiveness_param=0.00):
+            risk_penalty=0.007, session_time=20, aggressiveness_param=0.00):
         """
         Constructor for the Bot
         :param account: Account name
@@ -80,7 +83,19 @@ class CAPMBot(Agent):
         self._reactive_orders = []
         self._num_orders_sent = 0
 
+        # market maker bot
+        self._mm_orders = {}
+        self._num_active_mm_orders = 0
+
+    @staticmethod
+    def _initialise_custom_log():
+        logging.getLogger("agent").setLevel(15)
+        for handler in logging.getLogger("agent").handlers:
+            handler.setLevel(15)
+
     def initialised(self):
+
+        self._initialise_custom_log()
 
         # Extract payoff distribution for each security
         for market_id, market_info in self.markets.items():
@@ -92,34 +107,44 @@ class CAPMBot(Agent):
             self._payoffs[security] = [int(a) for a in description.split(",")]
 
         # bot starts off as a market maker FOR NOW IT IS REACTIVE FOR TESTING
-        self._bot_type = BotType.REACTIVE
+        self._bot_type = BotType.MARKET_MAKER
+        # self._bot_type = BotType.REACTIVE
+
+        # REACTIVE BOT =======================================================
 
         # if bot is reactive, then get best orders every one seconds
         self.execute_periodically_conditionally(
             self._reactive_strategy,
             WAIT_2_SECOND,
-            self._is_bot_reactive)
-
-        # if bot is market maker, create profitable orders every three seconds
-        self.execute_periodically_conditionally(
-            self._market_making,
-            WAIT_3_SECONDS,
-            self._is_bot_mm)
+            self._is_bot_reactive
+        )
 
         # cancelling stale orders every 6 seconds if bot is REACTIVE
         self.execute_periodically_conditionally(
             self._cancel_my_orders,
             WAIT_6_SECONDS,
-            self._is_bot_reactive)
+            self._is_bot_reactive
+        )
 
-        # sell notes if not enough cash every 10 seconds
-        self.execute_periodically(
-            self._sell_notes,
-            WAIT_6_SECONDS
+        # # sell notes if not enough cash every 10 seconds
+        # self.execute_periodically(
+        #     self._sell_notes,
+        #     WAIT_6_SECONDS
+        # )
+
+        # MARKET MAKER BOT ===================================================
+
+        # if bot is market maker, create profitable orders every three seconds
+        self.execute_periodically_conditionally(
+            self._market_making_strategy,
+            WAIT_6_SECONDS,
+            self._is_bot_mm
         )
 
         self.inform(f"Market IDs: {self._market_ids}")
         self.inform(f"Payoffs   : {self._payoffs}")
+
+        return
 
     # conditions for periodic orders, for some reason only works properly
     # with function pointers
@@ -128,6 +153,166 @@ class CAPMBot(Agent):
 
     def _is_bot_mm(self):
         return self._bot_type == BotType.MARKET_MAKER
+
+    def _market_making_strategy(self):
+        """
+        MARKET MAKER STRATEGY
+        =====================
+        Simulate acquiring/disposing off one unit of each security to calculate
+        the change in performance because of this balancing.
+
+        This change in performance is determined to be the price the security
+        should by bought/sold for (accounts for changing variance and
+        changing expected return)
+
+        That price is then adjusted by a PROFIT MARGIN, and submitted to the
+        market
+        """
+        # do nothing if sending through orders
+        if self._waiting or (len(self._asset_units.keys()) == 0):
+            return
+
+        # if any previous orders have traded, then cancel all orders,
+        # recalculate mm orders. If none have traded, do nothing
+        counter = 0
+        for fm_id, order in Order.current().items():
+            if order.mine:
+                counter += 1
+
+        # if number of current orders is the same as last recorded active
+        # orders, then do nothing
+        if counter > 0 and counter == self._num_active_mm_orders:
+            return
+
+        # if the current number of active orders is not the same as last
+        # recorded active orders - means some orders traded, so reset
+        if counter != self._num_active_mm_orders:
+            self._cancel_my_orders()
+            self._num_active_mm_orders = 0
+
+        self._mm_orders = {}
+
+        # acquire list of all securities
+        securities = list(self._payoffs.keys())
+        self.inform(securities)
+
+        # for each security, determine the prices of valid buys and sells that
+        # can be created in the market
+        order = Order.create_new()
+        order.price = 0
+
+        for security in securities:
+            # self.inform(f"{security}, {Market(self._market_ids[security])}")
+            order.market = Market(self._market_ids[security])
+
+            # create quasi buy order
+            order.order_side = OrderSide.BUY
+            self.is_portfolio_optimal([order])
+
+            # create quasi sell order
+            order.order_side = OrderSide.SELL
+            self.is_portfolio_optimal([order])
+
+        # filter out orders from potential orders that can't be executed
+        # due to not enough cash / units
+        self._remove_invalid_mm_orders()
+
+        # # send valid market maker orders in the market
+        if not self._waiting:
+            self._waiting = True
+            self._send_valid_mm_orders()
+
+        self.inform(f"All potential performances: {self._mm_orders}")
+
+    def _send_valid_mm_orders(self):
+        """
+        Creates and executes list of valid market maker orders
+        :return: List[Order] of valid orders
+        """
+        for key in self._mm_orders:
+
+            order = Order.create_new()
+            order.market = Market(self._market_ids[key[0]])
+            price_tick = order.market.price_tick
+            #
+            order.price = self._mm_orders[key] - \
+                          (self._mm_orders[key] % price_tick)
+            #
+            order.order_side = key[-1]
+            order.order_type = OrderType.LIMIT
+            order.units = 1
+            #
+            if order.market.min_price <= order.price <= order.market.max_price:
+                self._order_id += 1
+                self._num_orders_sent += 1
+                self._num_active_mm_orders += 1
+                self.send_order(order)
+        return
+
+    def _remove_invalid_mm_orders(self):
+        """
+        Removes invalid market maker orders from list of potential orders
+        Records the price adjusted by the profit margin of the orders
+        """
+
+        # track invalid order keys
+        to_del = []
+
+        for key in self._mm_orders.keys():
+            self._mm_orders[key] = \
+                int(100*abs(self._current_performance - self._mm_orders[key]))
+
+            # if we need to create a buy order
+            if key[-1] == OrderSide.BUY:
+                self._mm_orders[key] -= PROFIT_MARGIN
+
+                if self._mm_orders[key] > self._cash_available:
+                    # self.inform(f"Can't do {key}, {self._mm_orders[key]}")
+                    to_del.append(key)
+
+            # if we need to create a sell order
+            else:
+                self._mm_orders[key] += PROFIT_MARGIN
+
+        for key in to_del:
+            del self._mm_orders[key]
+
+    def get_potential_performance(self, orders: List[Order]):
+        """
+        Returns the portfolio performance if the given list of orders is
+        executed. The performance as per the following formula: Performance
+        = ExpectedPayoff - b * PayoffVariance, where b is the penalty for
+        risk.
+        :param orders      : list of orders to be simulated on portfolio
+        :return performance: percentage increase in portfolio performance
+        """
+
+        cash = self._cash_settled
+        units = self._asset_units.copy()
+
+        # simulate list of orders executing
+        for order in orders:
+            # for a buy order that exists in the market, we sell to it
+            if order.order_side == OrderSide.BUY:
+                cash += order.price
+                units[order.market.item] -= 1
+
+            # for a sell order, we buy from it
+            else:
+                cash -= order.price
+                units[order.market.item] += 1
+
+        # self.inform(f"Cash: {cash}, Units: {units}")
+        x = list(units.values())
+
+        variance = self._get_portfolio_variance(x)
+
+        performance = self._portfolio_performance(
+            self._get_expected_return(x, cash),
+            self._risk_penalty,
+            variance)
+
+        return performance
 
     def _sell_notes(self):
 
@@ -184,10 +369,6 @@ class CAPMBot(Agent):
                 self.send_order(cancel_order)
                 self._waiting = False
 
-    def _market_making(self):
-        if self._waiting:
-            return
-
     @staticmethod
     def _portfolio_performance(exp_return, risk_penalty, variance):
         """
@@ -229,8 +410,8 @@ class CAPMBot(Agent):
         return np.dot(np.dot(weights, covar_matrix),
                       weights.transpose())
 
-    # @staticmethod
-    def _get_best_bid_ask(self):
+    @staticmethod
+    def _get_best_bid_ask():
         """
         Walks the order book and determines what the best bid and asks are
         for each market
@@ -318,19 +499,40 @@ class CAPMBot(Agent):
         # for each order in this set, test if profitable
         for order_set in new_set:
 
-            list_orders = list(order_set)
+            if self._is_bot_reactive():
+                list_orders = list(order_set)
+            else:
+                list_orders = [order_set]
 
+            # self.inform(f"Order set: {list_orders}")
             # check if have enough cash/units for order_set
             if self.check_if_enough_assets(list_orders):
 
                 # check if performance of order_set > current performance
-                if self.get_potential_performance(list_orders) \
-                        > self._aggressiveness_param * \
-                        self._current_performance:
+                performance = self.get_potential_performance(list_orders)
 
-                    # when the first profitable order set found, stop
-                    self._reactive_orders = list_orders
-                    return False
+                # market maker bot
+                if self._is_bot_mm():
+
+                    # change order side to the action WE will be taking
+                    if list_orders[0].order_side == OrderSide.BUY:
+                        os = OrderSide.SELL
+                    else:
+                        os = OrderSide.BUY
+
+                    key = (list_orders[0].market.item,
+                           os)
+                    self._mm_orders[key] = performance
+
+                # reactive bot
+                else:
+                    if performance > self._aggressiveness_param * \
+                            self._current_performance:
+
+                        # when the first profitable order set found, stop
+                        self._reactive_orders = list_orders
+
+                        return False
 
         return True
 
@@ -352,6 +554,8 @@ class CAPMBot(Agent):
                 to_spend += order.price
 
             else:
+                # self.inform(f"{self._asset_units}")
+                # self.inform(f"{order}")
                 # if reached the shorting quota, then invalid order set
                 if self._asset_units[order.market.item] == \
                         self._short_units_allowed[order.market.item]:
@@ -397,43 +601,6 @@ class CAPMBot(Agent):
             self._order_id += 1
             self.send_order(new_order)
 
-    def get_potential_performance(self, orders: List[Order]):
-        """
-        Returns the portfolio performance if the given list of orders is
-        executed. The performance as per the following formula: Performance
-        = ExpectedPayoff - b * PayoffVariance, where b is the penalty for
-        risk.
-        :param orders      : list of orders to be simulated on portfolio
-        :return performance: percentage increase in portfolio performance
-        """
-
-        cash = self._cash_settled
-        units = self._asset_units.copy()
-
-        # simulate list of orders executing
-        for order in orders:
-            # for a buy order that exists in the market, we sell to it
-            if order.order_side == OrderSide.BUY:
-                cash += order.price
-                units[order.market.item] -= 1
-
-            # for a sell order, we buy from it
-            else:
-                cash -= order.price
-                units[order.market.item] += 1
-
-        # self.inform(f"Cash: {cash}, Units: {units}")
-        x = list(units.values())
-
-        variance = self._get_portfolio_variance(x)
-
-        performance = self._portfolio_performance(
-            self._get_expected_return(x, cash),
-            self._risk_penalty,
-            variance)
-
-        return performance
-
     @staticmethod
     def _duplicates_in_list(list_of_orders):
         """
@@ -467,10 +634,6 @@ class CAPMBot(Agent):
     def received_orders(self, orders: List[Order]):
         # seems to be called before received holdings, so don't calculate
         # the portfolio variance here! As this has old number of units
-
-        # # Market maker orders
-        # # simulate all 4 A B C note, price 0 and change in performance is the
-        # # max price to be paid. Try buy and sell and see if any can be exec
         return
 
     def received_session_info(self, session: Session):
@@ -527,7 +690,7 @@ if __name__ == "__main__":
     FM_ACCOUNT = "ardent-founder"
     FM_EMAIL = "s.mann4@student.unimelb.edu.au"
     FM_PASSWORD = "921322"
-    MARKETPLACE_ID = 1054  # replace this with the marketplace id
+    MARKETPLACE_ID = 1017  # replace this with the marketplace id
 
     # risk penalty based on my student ID
     bot = CAPMBot(FM_ACCOUNT, FM_EMAIL, FM_PASSWORD, MARKETPLACE_ID,
